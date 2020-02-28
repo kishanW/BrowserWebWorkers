f537b61245cf0510998d8d59490480e8fc9033ba
Kishan Wanigasinghe <radheekakishan@gmail.com>
2020-02-28 09:29:21 -0500

    cleanup

--- site.js
+++ site.js
@@ -1,122 +1,126 @@
 var ww = {};
 
-$(document).ready(function () {
-    if (window.Worker) {
-        ww.AddMessage("web worker is supported");
+$(document).ready(function() {
+  if (window.Worker) {
+    ww.AddMessage("web worker is supported");
 
-        var myWorker = new Worker('worker.js');
-        var queueworker = new Worker('queueworker.js');
+    //initialize the workers
+    var myWorker = new Worker("worker.js");
+    var queueworker = new Worker("queueworker.js");
 
-        queueworker.onmessage = function (e) {
-            console.log('Message received from worker');
-            console.log(e.data);
+    queueworker.onmessage = function(e) {
+      console.log("Message received from worker");
+      console.log(e.data);
 
-            var message = e.data[0];
-            var callback = e.data[1];
+      var message = e.data[0];
+      var callback = e.data[1];
 
-            ww.AddMessage("<b>" + message + "</b>")
+      ww.AddMessage("<b>" + message + "</b>");
+    };
+    window.queueworker = queueworker;
+
+    myWorker.onmessage = function(e) {
+      console.log("Message received from worker");
+      console.log(e.data);
+
+      var message = e.data[0];
+      var callback = e.data[1];
+
+      if (callback) {
+        if (callback === "AddMessage") {
+          ww.AddMessage(message);
+        } else if (callback === "SetConnectionBadge") {
+          ww.SetConnectionBadge(message);
         }
-        window.queueworker = queueworker;
+      } else {
+        ww.AddMessage("<b>" + e.data + "</b>");
+      }
+    };
+    window.myworker = myWorker;
+  }
 
-
-        myWorker.onmessage = function (e) {
-            console.log('Message received from worker');
-            console.log(e.data);
-
-            var message = e.data[0];
-            var callback = e.data[1];
-
-            if (callback) {
-                if (callback === "AddMessage") {
-                    ww.AddMessage(message);
-                } else if (callback === "SetConnectionBadge") {
-                    ww.SetConnectionBadge(message);
-                }
-            } else {
-                ww.AddMessage("<b>" + e.data + "</b>")
-            }
-        }
-        window.myworker = myWorker;
-    }
-
-
-    ww.AddOfflineSupport();
+  //register the service worker
+  ww.AddOfflineSupport();
 });
 
-ww.SetConnectionBadge = function (status) {
-    var badge = $("#connectionBadge");
-    badge.attr("online", status);
+ww.SetConnectionBadge = function(status) {
+  var badge = $("#connectionBadge");
+  badge.attr("online", status);
 
-    ww.AddMessage(status ? "App is online" : "App is offline");
-}
+  ww.AddMessage(status ? "App is online" : "App is offline");
+};
 
+ww.AddMessage = function(messageString) {
+  var messageList = $("#messages");
+  var newMessage = $("<li class='list-group-item'>" + messageString + "</li>");
 
-ww.AddMessage = function (messageString) {
-    var messageList = $("#messages");
-    var newMessage = $("<li class='list-group-item'>" + messageString + "</li>");
+  messageList.prepend(newMessage);
+};
 
-    messageList.prepend(newMessage);
-}
-
-$(document).on("click", "#clearmessages", function () {
-    var messageList = $("#messages");
-    messageList.children().each(function () {
-        $(this).slideUp(function () {
-            $(this).remove();
-        });
+$(document).on("click", "#clearmessages", function() {
+  var messageList = $("#messages");
+  messageList.children().each(function() {
+    $(this).slideUp(function() {
+      $(this).remove();
     });
+  });
 });
 
-$(document).on("click", "#sendmessage", function () {
-    ww.AddMessage("sending message");
-    window.setTimeout(500);
+$(document).on("click", "#sendmessage", function() {
+  ww.AddMessage("sending message");
+  window.setTimeout(500);
 
-    var messageToSend = $("#messageInput").val();
-    window.myworker.postMessage(["postmessage", messageToSend, "AddMessage"]);
+  var messageToSend = $("#messageInput").val();
+  window.myworker.postMessage(["postmessage", messageToSend, "AddMessage"]);
 
-    $("#messageInput").val("");
+  $("#messageInput").val("");
 });
 
-$(document).on("click", "#checkconnection", function () {
-    ww.AddMessage("checking connection");
-    window.setTimeout(500);
+$(document).on("click", "#checkconnection", function() {
+  ww.AddMessage("checking connection");
+  window.setTimeout(500);
 
-    window.myworker.postMessage(["checkconnection", "new message", "SetConnectionBadge"]);
+  window.myworker.postMessage([
+    "checkconnection",
+    "new message",
+    "SetConnectionBadge"
+  ]);
 });
 
+$(document).on("click", "#togglequeue", function() {
+  ww.AddMessage("toggling queue");
+  window.setTimeout(500);
 
-$(document).on("click", "#togglequeue", function () {
-    ww.AddMessage("toggling queue");
-    window.setTimeout(500);
-
-    window.queueworker.postMessage(["toggleprocess", true, "SetConnectionBadge"]);
+  window.queueworker.postMessage(["toggleprocess", true, "SetConnectionBadge"]);
 });
 
-$(document).on("change", "#autonetworkcheck", function () {
-    window.myworker.postMessage(["autoconnectioncheck", "new message", "SetConnectionBadge"]);
+$(document).on("change", "#autonetworkcheck", function() {
+  window.myworker.postMessage([
+    "autoconnectioncheck",
+    "new message",
+    "SetConnectionBadge"
+  ]);
 });
 
-$(document).on("click", "#wwQueueHugeJob", function () {
-    window.myworker.postMessage(["hugetask", "new message", "AddMessage"]);
+$(document).on("click", "#wwQueueHugeJob", function() {
+  window.myworker.postMessage(["hugetask", "new message", "AddMessage"]);
 });
 
-$(document).on("click", "#nonWorkerJobQueueToggle", function () {
-    ww.AddMessage("starting non web worker js task ");
-    for (var i = 0; i < 1000000000; i++) {
-        Math.random();
-    }
-    ww.AddMessage("completed non web worker js task");
+$(document).on("click", "#nonWorkerJobQueueToggle", function() {
+  ww.AddMessage("starting non web worker js task ");
+  for (var i = 0; i < 1000000000; i++) {
+    Math.random();
+  }
+  ww.AddMessage("completed non web worker js task");
 });
 
-ww.AddOfflineSupport = function(){
-    if ('serviceWorker' in navigator) {
-        navigator.serviceWorker
-            .register('/offlineworker.js')
-            .then(function () { 
-                console.log("Service Worker Registered"); 
-            });
-    }
-    else{
-        console.log("Cannot add offline support - service worker not supported.")
-    }
-}
+ww.AddOfflineSupport = function() {
+  //register the service workers (only one available per site!)
+  if ("serviceWorker" in navigator) {
+    navigator.serviceWorker.register("/offlineworker.js").then(function() {
+      console.log("Service Worker Registered");
+    });
+  } else {
+    console.log("Cannot add offline support - service worker not supported.");
+  }
+};
--- worker.js
+++ worker.js
@@ -1,56 +1,62 @@
 var workerSpace = {
-    IsAutoCheckOn: false,
-    AutoNetworkCheckInterval: {}
+  IsAutoCheckOn: false,
+  AutoNetworkCheckInterval: {}
 };
 
-onmessage = function (e) {
-    var requestType = e.data[0];
-    var message = e.data[1];
-    var callback = e.data[2];
+onmessage = function(e) {
+  var requestType = e.data[0];
+  var message = e.data[1];
+  var callback = e.data[2];
 
-    if (requestType === "checkconnection") {
-        var isOnline = self.navigator.onLine;
-        var postbackMsg = "We are " + (isOnline ? "Online" : "Offline");
-        console.log(postbackMsg);
-        PostBackMessage(isOnline, callback);
-    }
-    if (requestType === "autoconnectioncheck") {
-        workerSpace.IsAutoCheckOn = !workerSpace.IsAutoCheckOn;
-        if (!workerSpace.IsAutoCheckOn) {
-            clearInterval(workerSpace.AutoNetworkCheckInterval);
-            PostBackMessage("Stopped auto network check", "AddMessage");
-            return;
-        }
+  console.log("[worker.js] request message received: ", e);
 
-        workerSpace.AutoNetworkCheckInterval = setInterval(function () {
-            var isOnline = self.navigator.onLine;
-            var postbackMsg = "We are " + (isOnline ? "Online" : "Offline");
-            console.log(postbackMsg);
-            PostBackMessage(isOnline, callback);
-        }, 500)
-    } else if (requestType === "postmessage") {
-        console.log("message received:");
-        console.log(message);
-        console.log("\n\n");
-        var postbackMsg = "message received --- " + message;
-        PostBackMessage(postbackMsg, callback);
-    } else if (requestType === "hugetask") {
-        PostBackMessage("worker.js - starting huge task", callback);
-        console.log("worker.js - starting huge task");
-        for (var i = 0; i < 1000000000; i++) {
-            Math.random();
-            //PostBackMessage("worker.js - status - " + i, callback);
-        }
-        console.log("worker.js - huge task done.");
-        console.log("\n\n");
-        var postbackMsg = "worker.js completed huge task";
-        PostBackMessage(postbackMsg, callback);
+  if (requestType === "checkconnection") {
+    var isOnline = self.navigator.onLine;
+    var postbackMsg = "We are " + (isOnline ? "Online" : "Offline");
+    console.log(postbackMsg);
+    PostBackMessage(isOnline, callback);
+    return;
+  }
+  if (requestType === "autoconnectioncheck") {
+    workerSpace.IsAutoCheckOn = !workerSpace.IsAutoCheckOn;
+    if (!workerSpace.IsAutoCheckOn) {
+      clearInterval(workerSpace.AutoNetworkCheckInterval);
+      PostBackMessage("Stopped auto network check", "AddMessage");
+      return;
     }
 
+    workerSpace.AutoNetworkCheckInterval = setInterval(function() {
+      var isOnline = self.navigator.onLine;
+      var postbackMsg = "We are " + (isOnline ? "Online" : "Offline");
+      console.log(postbackMsg);
+      PostBackMessage(isOnline, callback);
+    }, 500);
 
-}
+    return;
+  } else if (requestType === "postmessage") {
+    console.log("message received:");
+    console.log(message);
+    console.log("\n\n");
+    var postbackMsg = "message received --- " + message;
+    PostBackMessage(postbackMsg, callback);
 
+    return;
+  } else if (requestType === "hugetask") {
+    PostBackMessage("worker.js - starting huge task", callback);
+    console.log("worker.js - starting huge task");
+    for (var i = 0; i < 1000000000; i++) {
+      Math.random();
+      //PostBackMessage("worker.js - status - " + i, callback);
+    }
+    console.log("worker.js - huge task done.");
+    console.log("\n\n");
+    var postbackMsg = "worker.js completed huge task";
+    PostBackMessage(postbackMsg, callback);
+
+    return;
+  }
+};
 
 function PostBackMessage(message, callback) {
-    postMessage([message, callback]);
+  postMessage([message, callback]);
 }
